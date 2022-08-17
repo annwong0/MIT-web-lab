e224508737da7593ba2acc23a8d10d49601df878
alicepywong <alicepywong715@gmail.com>
2022-08-17 17:03:34 +0800

    workshop 2

---
+++ w2-catbook-react/client/src/components/modules/CatHappiness.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import "./CatHappiness.css";
+
+/**
+ * Component that renders cat happiness
+ *
+ * Props
+ * @param {int} catHappiness is how happy your cat is
+ */
+const CatHappiness = (props) => {
+  // TODO Step 1: Initialize state of CatHappiness
+  return (
+    <div className="CatHappiness-container">
+      <div className="CatHappiness-story">
+        <p className="CatHappiness-storyContent">
+          {/* TODO Step 1: insert happiness counter here!*/
+          props.catHappiness
+          }
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CatHappiness;
